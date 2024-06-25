267cfd48a4e8914076acec36bb2724a64188e203
s3r10us3r <tradeckimikolaj7@gmail.com>
2024-06-25 18:51:14 +0200

    Added question factory tests

--- OnlineSurveyTool.Server.Services.Test/SurveyServices/Helpers/QuestionFactoryTest.cs
+++ OnlineSurveyTool.Server.Services.Test/SurveyServices/Helpers/QuestionFactoryTest.cs
@@ -1,3 +1,4 @@
+using NuGet.Frameworks;
 using OnlineSurveyTool.Server.DAL.Models;
 using OnlineSurveyTool.Server.Services.SurveyServices.DTOs;
 using OnlineSurveyTool.Server.Services.SurveyServices.Helpers;
@@ -9,16 +10,100 @@ namespace OnlineSurveyTool.Server.Services.Test.SurveyServices.Helpers;
 public class QuestionFactoryTest
 {
     private IQuestionFactory _questionFactory;
+    private ISurveyValidator _surveyValidator;
     
     [OneTimeSetUp]
     public void SetUp()
     {
         _questionFactory = new QuestionFactory();
+        _surveyValidator = new SurveyValidator();
     }
 
-    private void ShouldCorrectlyConvertQuestionToType<T>(Question question) where T : QuestionBase
+    [Test]
+    public void ShouldCorrectlyConvertToSingleChoice()
     {
-        var converted = _questionFactory.MakeQuestionBase(question);
-        Assert.That(converted, Is.TypeOf<T>());
+        var question = new Question()
+        {
+            CanBeSkipped = true,
+            ChoiceOptions =
+                [new() { Id = 1, Number = 1, Value = "Option 1" }, new() { Id = 2, Number = 2, Value = "Option 2" }],
+            Id = 1,
+            Number = 1,
+            Type = QuestionType.SingleChoice
+        };
+        ShouldCorrectlyConvert<SingleChoiceQuestionDTO>(question);
+    }
+
+    [Test]
+    public void ShouldCorrectlyConvertToMultipleChoice()
+    {
+        var question = new Question()
+        {
+            CanBeSkipped = true,
+            Minimum = 1,
+            Maximum = 2,
+            ChoiceOptions =
+                [new() { Id = 1, Number = 1, Value = "Option 1" }, new() { Id = 2, Number = 2, Value = "Option 2" }],
+            Id = 1,
+            Number = 1,
+            Type = QuestionType.MultipleChoice
+        };
+        ShouldCorrectlyConvert<MultipleChoiceQuestionDTO>(question);
+    }
+
+    [Test]
+    public void ShouldCorrectlyConvertToNumericalInteger()
+    {
+        var question = new Question()
+        {
+            CanBeSkipped = true,
+            Minimum = 1,
+            Maximum = 10,
+            Id = 1,
+            Number = 1,
+            Type = QuestionType.NumericalInteger
+        };
+        ShouldCorrectlyConvert<NumericalIntegerQuestionDTO>(question);
+    }
+    
+    [Test]
+    public void ShouldCorrectlyConvertToNumericalDouble()
+    {
+        var question = new Question()
+        {
+            CanBeSkipped = true,
+            Minimum = 1,
+            Maximum = 10,
+            Id = 1,
+            Number = 1,
+            Type = QuestionType.NumericalDouble
+        };
+        ShouldCorrectlyConvert<NumericalDoubleQuestionDTO>(question);
+    }
+
+    [Test]
+    public void ShouldCorrectlyConvertToTextual()
+    {
+        var question = new Question()
+        {
+            CanBeSkipped = true,
+            Minimum = 1,
+            Maximum = 1000,
+            Id = 1,
+            Number = 1,
+            Type = QuestionType.Textual
+        };
+        ShouldCorrectlyConvert<TextualQuestionDTO>(question);
+    }
+    
+    private void ShouldCorrectlyConvert<T>(Question question) where T : QuestionBase
+    {
+        var result = _questionFactory.MakeQuestionBase(question);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.TypeOf<T>());
+            Assert.That(_surveyValidator.ValidateQuestion(result, out var message), Is.True, message);
+        });
     }
 }
